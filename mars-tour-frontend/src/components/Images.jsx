12aa6ffa9addcece555c8dd7277bbec31f25e4b3
david-c-oliveros <david3oliveros@gmail.com>
2022-02-11 16:51:24 -0500

    Bug fix

--- mars-tour-frontend/src/components/Images.jsx
+++ mars-tour-frontend/src/components/Images.jsx
@@ -6,13 +6,13 @@ import { Link, useSearchParams } from 'react-router-dom'
 import testData from '../test-data'
 
 const TEST = true
-let debug_fetchCounter = 0
 
 function Images(props)
 {
     const [searchParams, setSearchParams] = useSearchParams()
     const craftName = searchParams.get('craft')
     const [craft, setCraft] = useState(props.rovers[craftName])
+    const [debug_fetchCount, setDebug_fetchCount] = useState(0)
 
     const handleFetch = async (date) => {
         const key = process.env.REACT_APP_API_KEY
@@ -25,7 +25,6 @@ function Images(props)
         if (res.status >= 200 && res.status <= 299)
         {
             const data = await res.json()
-            debug_fetchCounter++
             return data
         } else {
             return -1
@@ -48,6 +47,7 @@ function Images(props)
     // Tries to make a fetch call for the most recent images
     // If none are returned, it makes the call again but with the previous date
     const retreiveRecentImages = async (date, limiter) => {
+        console.log(date)
         const dateString = date.toISOString().substring(0, 10)
         const data = await handleFetch(dateString)
 
@@ -60,7 +60,7 @@ function Images(props)
 
         // If the array of photos returned is empty,
         // call the function again with the previous date
-        if (data.photos.length === 0 && limiter < 50)
+        if (data.photos.length === 0 && limiter < 10)
         {
             date.setDate(date.getDate() - 1)
             retreiveRecentImages(date, ++limiter)
@@ -76,7 +76,6 @@ function Images(props)
         let date
         if (craft.active)
         {
-            console.log('active')
             date = new Date()
         }
         else
@@ -84,7 +83,6 @@ function Images(props)
             date = new Date(craft.missionEnd)
         }
 
-        date = new Date()
         const dateString = date.toISOString().substring(0, 10)
 
         if (TEST)
@@ -92,7 +90,6 @@ function Images(props)
         else
             retreiveRecentImages(date, 0)
 
-        console.log('number of fetches:', debug_fetchCounter)
     }, [])
 
     if (props.images.length === 0)
