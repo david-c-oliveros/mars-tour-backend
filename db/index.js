d168682c817860822c209fa98fbd1842953bf8ae
david-c-oliveros <david3oliveros@gmail.com>
2022-02-16 13:25:08 -0500

    debug

--- db/index.js
+++ db/index.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
 
 const MONGO_URI = process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/mars'
+console.log(MONGO_URI)
 
 mongoose.connect(MONGO_URI, () => {
     console.log('connected to MongoDB')
